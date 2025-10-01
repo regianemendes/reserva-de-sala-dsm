0d20fb1b083a31bf83548c4dc1836f0a7b5e3c60
regianemendes <regianemendesfatec@gmail.com>
2025-10-01 19:44:57 -0300

    first commit

---
+++ reserva-de-sala-dsm/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using reserva_de_sala_dsm.Models;
+
+namespace reserva_de_sala_dsm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ reserva-de-sala-dsm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reserva_de_sala_dsm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
